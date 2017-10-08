eb11379d4645d587cdd25f880688d9ed4727c1bc
Mubaris NK <mubarishassannk@gmail.com>
2017-10-08 16:12:50 +0530

    New Method

--- main.py
+++ main.py
@@ -1,111 +1,140 @@
+import requests
 from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-import pickle
+import networkx as nx
+from networkx.readwrite import json_graph
+import math
+import json
+from collections import defaultdict
 
-url = 'https://www.wikiwand.com/en/Flash_(comics)'
-parent_url = 'https://www.wikiwand.com'
+main_url = 'https://en.wikipedia.org'
+url = 'https://en.wikipedia.org/wiki/Flash_(Barry_Allen)'
 
-'''wd = webdriver.PhantomJS()
-wd.get(url)
+def generate_graph(url, graph, l=1, max_nodes=50):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    header = str(soup.find('h1', {'id': 'firstHeading', 'class': 'firstHeading'}).text).strip()
+    try:
+        soup.find('div', {'class': 'reflist'}).decompose()
+    except:
+        pass
+    content = soup.find('div', {'class': 'mw-parser-output'})
+    links = content.findAll('a', href=True, title=True)
+    for i, el in enumerate(links):
+        if not str(el.get('href')).strip().startswith('/wiki/'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Category:'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Help:'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Wikipedia:'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Special:'):
+            del links[i]
+        if str(el.get('href')).strip().startswith('/wiki/List_of'):
+            del links[i]
+        if str(el.get('title')).strip().endswith('(disambiguation)'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Edit section'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Portal:'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Template:'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('Enlarge'):
+            del links[i]
+        if str(el.get('title')).strip().startswith('List of'):
+            del links[i]
+        if 'mw-redirect' in str(el.get('class')).strip():
+            del links[i]
 
-WebDriverWait(wd, 10).until(
-    EC.visibility_of_element_located((By.CLASS_NAME, "read_more_link")))
-
-html_page = wd.page_source
-wd.quit()
-
-soup = BeautifulSoup(html_page, 'html.parser')
-
-root = soup.find(class_='firstHeading')
-
-root_title = str(root.text).strip()
-
-nodes = []
-nodes.append(root_title)
-
-relatives = soup.find_all('a', {'class': 'read_more_link'})
-relatives = relatives[:3]
-
-count = 1
-
-for i, el in enumerate(relatives):
-    url = parent_url + el['href']
-    wd = webdriver.PhantomJS()
-    wd.get(url)
-
-    WebDriverWait(wd, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "read_more_link")))
-
-    html_page = wd.page_source
-    wd.quit()
-
-    soup = BeautifulSoup(html_page, 'html.parser')
-
-    head = soup.find(class_='firstHeading')
-
-    title = str(head.text).strip()
-    nodes.append(title)
-    count += 1
-    if count > 100:
-        break'''
-
-def get_relatives(url):
-    wd = webdriver.PhantomJS()
-    wd.get(url)
-
-    WebDriverWait(wd, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "read_more_link")))
-
-    html_page = wd.page_source
-    wd.quit()
-
-    soup = BeautifulSoup(html_page, 'html.parser')
-
-    head = soup.find(class_='firstHeading')
-
-    title = str(head.text).strip()
-    relatives = soup.find_all('a', {'class': 'read_more_link'})
-    urls = relatives[:3]
-    url_list = []
-    title_list = []
-    for i, el in enumerate(urls):
-        url = parent_url + el['href']
-        titl = el.find('span')
-        titl = str(titl.text)
-        title_list.append(titl)
-        url_list.append(url)
-    return title, title_list, url_list
-
-title, title_list, url_list = get_relatives(url)
-
-graph = {
-    title: title_list
-}
-
-count = 1
-urls = []
-while count < 15:
-    for i, el in enumerate(url_list):
-        try:
-            title, title_list, rel_url_list = get_relatives(el)
-        except:
+    pairs = []
+    for i, el in enumerate(links):
+        href = main_url + str(el.get('href')).strip()
+        title = str(el.get('title')).strip()
+        if not href.startswith('https://en.wikipedia.org/wiki/'):
             continue
-        if title not in graph.keys():
-            graph[title] = title_list
-            urls.extend(rel_url_list)
-            print(graph)
-            filename = 'graph.pkl'
-            file = open(filename, 'wb')
-            pickle.dump(graph, file)
-            file.close()
-    url_list = urls
-    urls = []
-    count += 1
+        if title.startswith('Category:'):
+            continue
+        if title.startswith('Help:'):
+            continue
+        if title.startswith('Wikipedia:'):
+            continue
+        if title.startswith('Special:'):
+            continue
+        if href.startswith('https://en.wikipedia.org/wiki/List_of'):
+            continue
+        if title.endswith('(disambiguation)'):
+            continue
+        if title.startswith('Edit section'):
+            continue
+        if title.startswith('List of'):
+            continue
+        if title.startswith('Portal:'):
+            continue
+        if title.startswith('Template:'):
+            continue
+        if title.startswith('Enlarge'):
+            continue
+        if 'mw-redirect' in str(el.get('class')).strip():
+            continue
+        if '#' in href:
+            index = href.index('#')
+            href = href[:index]
+        pairs.append((title, href))
 
-filename = 'graph.pkl'
-file = open(filename, 'wb')
-pickle.dump(graph, file)
-file.close()
+    fq = defaultdict(int)
+    for pair in pairs:
+        fq[pair] += 1
+    value_dict = dict(fq)
+    if header not in graph.nodes():
+        graph.add_node(header, url=url)
+    sorted_values = sorted(value_dict, key=lambda x:value_dict[x], reverse=True)
+    if max_nodes == -1:
+        max_nodes = math.inf
+    for i, el in enumerate(sorted_values):
+        if i < max_nodes and value_dict[el] >= l and (header, el[0]) not in graph.edges():
+            graph.add_edge(header, el[0], weight=value_dict[el])
+            graph.node[el[0]]['url'] = el[1]
+    return graph
+    
+
+'''def print_sorted(value_dict, l=1):
+    sorted_values = sorted(value_dict, key=lambda x:value_dict[x])
+    for k in sorted_values:
+        if value_dict[k] >= l:
+            print("{}: {}".format(k, value_dict[k]))
+
+#value_dict = generate_graph(url)
+#print_sorted(value_dict, l=1)
+print(generate_graph(url, {}))'''
+
+'''G = nx.MultiDiGraph()
+G = generate_graph(url, G)
+print(len(G.nodes()))
+print(G.nodes())
+for i, el in enumerate(G.nodes(data=True)):
+    if i != 0:
+        G = generate_graph(el[1]['url'], G)
+        print(len(G.nodes()))
+        print(G.nodes())'''
+
+def recursive_graph(url, depth=2, l=1, max_nodes=50):
+    G = nx.MultiDiGraph()
+    G = generate_graph(url, G, l=l, max_nodes=max_nodes)
+    print(0, 0, len(G.nodes()))
+    #print(G.nodes())
+    count = 1
+    while count <= depth:
+        for i, el in enumerate(G.nodes(data=True)):
+            try:
+                G = generate_graph(el[1]['url'], G, l=l, max_nodes=max_nodes)
+            except:
+                continue
+            print(count-1, i,len(G.nodes()))
+            #print(G.nodes())
+        count += 1
+    return G
+
+G = recursive_graph(url, max_nodes=-1)
+with open('output.json', 'w') as out:
+    out.write(json.dumps(json_graph.node_link_data(G)))
